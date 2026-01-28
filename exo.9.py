06d10caa84ee272186ed9c34d089369501415a2a
ukhedekar47-cmyk <ukhedekar47@gmail.com>
2026-01-28 09:56:13 +0530

    Add files via upload

---
+++ exo.9.py
@@ -0,0 +1,10 @@
+print("NAME: Aryan Khedekar, UIN: 251P107")
+
+
+
+import math
+
+def factorial(n):
+ return(math.factorial(n))
+num = int(input("Enter the  number: "))
+print("Factorial of", num, "is",factorial(num))
