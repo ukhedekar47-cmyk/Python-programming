0df97e3050d09ce2960a811d7ccd35816aac5c74
ukhedekar47-cmyk <ukhedekar47@gmail.com>
2026-01-28 09:53:00 +0530

    Add files via upload

---
+++ exo.1.py
@@ -0,0 +1,12 @@
+print("Uin: 251P107,Name: Aryan Khedekar")
+ 
+ 
+name=input("What is your name..?")
+hobby=input("What is your favourite hobby..?")
+food=input("What is favourite food..?")
+
+print("\nHello," +name+"!")
+print("It'sgreat that you enjoy" +hobby+ ".")
+print("I hope that you getn to eat some" +food+ " today!")
+print("Have an amzing day ," +name+ "!")           
+
