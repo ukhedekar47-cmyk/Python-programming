2372afc4ca31acb4df2fb5600043d19924a1b4cc
ukhedekar47-cmyk <ukhedekar47@gmail.com>
2026-01-28 09:54:38 +0530

    Add files via upload

---
+++ exo.4.py
@@ -0,0 +1,6 @@
+print("UIN: 251P107,NAME: Aryan Khedekar")
+principle=float(input("Enter the principle amount:"))
+rate=float(input("Enter te rate of interest:"))
+time=float(input("Enter the time period of years:"))
+simple_interest=(principle*rate*time)/100
+print("Simle Interest is:",simple_interest)
