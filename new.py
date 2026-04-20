b9bac96ead61785d9701ec73456cdbdfc8498990
ukhedekar47-cmyk <ukhedekar47@gmail.com>
2026-04-20 23:37:19 +0530

    Update new.py

--- new.py
+++ new.py
@@ -123,7 +123,18 @@ def main():
     else:
         print("Invalid Choice")
 
+# Electricity Bill Calculator (Slab Based)
 
+units = int(input("Enter electricity units: "))
+
+if units <= 100:
+    bill = units * 5
+elif units <= 200:
+    bill = (100 * 5) + (units - 100) * 7
+else:
+    bill = (100 * 5) + (100 * 7) + (units - 200) * 10
+
+print("Total Bill: ₹", bill)
 
 
 
